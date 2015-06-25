f96bb240b08430ad917ab755a9bf2e125f867dcc
Morley Zhi <morley@okcupid.com>
2015-06-25 19:03:43 -0400

    fix the unit tests (and test data) to work the way ES6 expects you to work

--- opt/test/index.js
+++ opt/test/index.js
@@ -11,36 +11,37 @@ describe('shortify', function() {
   it('should resolve module "foo"', function() {
     var foo = require('one/foo');
 
-    chai.expect(foo).to.equal('foo');
+    chai.expect(foo.foo).to.equal(1);
   });
 
   it('should resolve module "bar"', function() {
     var bar = require('two/../two/bar');
 
-    chai.expect(bar).to.equal('bar');
+    chai.expect(bar.bar).to.equal(1);
   });
 
   it('should resolve module "baz"', function() {
     var baz = require('../test/three/baz');
 
-    chai.expect(baz).to.equal('baz');
+    chai.expect(baz.baz).to.equal(1);
   });
 
 });
 
-
+// ES6 import doesn't work the same way as require,
+// so we expect something slightly different.
 describe('shortify in ES6', function() {
 
   it('should resolve module "foo"', function() {
-    chai.expect(fooImport).to.equal('foo');
+    chai.expect(fooImport).to.equal(1);
   });
 
   it('should resolve module "bar"', function() {
-    chai.expect(barImport).to.equal('bar');
+    chai.expect(barImport).to.equal(1);
   });
 
   it('should resolve module "baz"', function() {
-    chai.expect(bazImport).to.equal('baz');
+    chai.expect(bazImport).to.equal(1);
   });
 
 });
